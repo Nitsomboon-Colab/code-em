87a4caf3022801ec0399a894f43e137941505006
NutNuttaphong <88608009+NutNuttaphong@users.noreply.github.com>
2021-08-09 20:32:15 +0700

    add LandingLast1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Landing from "./view/Landing";
 import LandingGoal from "./components/LandingGoal.js";
 import LandingYell from "./components/LandingYell";
+import LandingLast1 from "./components/LandingLast1";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
-      <LandingYell />
+      <LandingLast1 />
     </>
   );
 }
